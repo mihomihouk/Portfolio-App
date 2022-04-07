42cbd3abff8dca6b5851cfc188ea15819ac3ac51
mihomihouk <mihomihouk@gmail.com>
2022-04-07 21:57:12 +0100

    Sidebar toggle

--- src/components/organisms/Header.jsx
+++ src/components/organisms/Header.jsx
@@ -1,39 +1,45 @@
 import React from 'react'
+import { useRecoilState } from "recoil"
+
+//hooks
+import { navbarState } from "../../hooks/NavbarState"
 
 //styles
-import { AppBar, IconButton, Toolbar, Typography, Avatar } from '@mui/material'
+import { AppBar, Box, IconButton, Toolbar, Avatar } from '@mui/material'
 import MenuIcon from '@mui/icons-material/Menu'
 
-const Header = (props) => {
-  const { pageTitle, handleDrawerToggle} = props
+const Header = () => {
+  const [ open, setOpen ] = useRecoilState(navbarState)
+
+  const handleDrawerToggle = () => {
+    setOpen (!open)
+  }
+
 
   return (
     <>
       <AppBar position="absolute" >
           <Toolbar>
-            <IconButton
-              edge="start"
-              color="inherit"
-              aria-label="open drawer"
-              sx={{
-                marginRight: '36px'
-              }}
-              onClick={handleDrawerToggle}
-            >
-              <MenuIcon />
-            </IconButton>
-            <Typography
-              component="h1"
-              variant="h6"
-              color="inherit"
-              noWrap
-              sx={{ flexGrow: 1 }}
-            >
-              {pageTitle}
-            </Typography>
-            <IconButton color="inherit">
-              <Avatar />
-            </IconButton>
+            <Box sx={{display:"flex", alignItems:"center", width:"100%"}}>
+              <Box sx={{width:"100%"}}>
+                <IconButton
+                  edge="start"
+                  color="inherit"
+                  aria-label="open drawer"
+                  sx={{
+                    marginRight: '36px'
+                  }}
+                  onClick={handleDrawerToggle}
+                >
+                  <MenuIcon />
+                </IconButton>
+              </Box>
+              <Box sx={{justifyContent:"flex-end"}}>
+                <IconButton color="inherit">
+                  <Avatar />
+                </IconButton>
+              </Box>
+            </Box>
           </Toolbar>
         </AppBar>
     </>
--- src/components/organisms/Sidebar.jsx
+++ src/components/organisms/Sidebar.jsx
@@ -1,65 +1,48 @@
 import React from 'react'
 
+import { useRecoilState } from "recoil"
+
+//hooks
+import { navbarState } from "../../hooks/NavbarState"
+
 //styles
-import { Divider, Drawer, Grid, List, ListItem, ListItemIcon, ListItemText, Toolbar, Typography } from '@mui/material'
+import {  Drawer, List, ListItem, ListItemIcon, ListItemText, Toolbar } from '@mui/material'
 import Dashboard from '@mui/icons-material/Dashboard'
 
-const Sidebar = (props) => {
-  
+const Sidebar = () => {
+  const [ open, setOpen ] = useRecoilState(navbarState)
 
-  const { open, handleDrawerToggle} = props;
+  const handleDrawerToggle = () => {
+    setOpen(!open)
+  }
 
   const drawer = (
     <>
       <Toolbar/>
       <List>
-        {['Dashboard', 'Calendar', 'Rota', 'Chat', 'Discussion'].map((text, index) => (
+        {['Home', 'Calendar', 'Chat', 'Discussion'].map(text => (
           <ListItem button key={text}>
             <ListItemIcon>
-              {index % 2 === 0 ? <Dashboard/> : <Dashboard />}
-            </ListItemIcon>
-            <ListItemText primary={text} />
-          </ListItem>
-        ))}
-      </List>
-      <Divider />
-      <List>
-        {['User Profile'].map((text, index) => (
-          <ListItem button key={text}>
-            <ListItemIcon>
-              {index % 2 === 0 ? <Dashboard /> : <Dashboard />}
+              <Dashboard/>
             </ListItemIcon>
             <ListItemText primary={text} />
           </ListItem>
         ))}
       </List>
     </>
-  );
+  )
 
   return (
         <>
-          {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
           <Drawer
             variant="temporary"
+            anchor="left"
             open={open}
             onClose={handleDrawerToggle}
-            ModalProps={{
-              keepMounted: true, // Better open performance on mobile.
-            }}
             sx={{
-              display: { xs: 'block', sm: 'none' },
+              display: 'block',
               '& .MuiDrawer-paper': { boxSizing: 'border-box'},
             }}
-          >
-            {drawer}
-          </Drawer>
-          <Drawer
-            variant="permanent"
-            sx={{
-              display: { xs: 'none', sm: 'block' },
-              '& .MuiDrawer-paper': { boxSizing: 'border-box'},
-            }}
-            open
           >
             {drawer}
           </Drawer>
