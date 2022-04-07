84554c10bcd73da37f1c9a832ee4e33a2bf05369
mihomihouk <mihomihouk@gmail.com>
2022-04-07 21:58:05 +0100

    Installed recoil

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,20 @@
 import { ScopedCssBaseline } from '@mui/material'
 import '../styles/globals.css'
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
+import { RecoilRoot } from "recoil"
+
+
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ScopedCssBaseline>
-      <Component {...pageProps} />
-    </ScopedCssBaseline>
+      <RecoilRoot>
+        <ScopedCssBaseline>
+          <LocalizationProvider dateAdapter={AdapterDateFns}>
+              <Component {...pageProps} />
+          </LocalizationProvider>
+        </ScopedCssBaseline>
+      </RecoilRoot>
     )
 }
 
