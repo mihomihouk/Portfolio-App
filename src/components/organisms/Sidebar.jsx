287e72ed97b749009cc253c6ff64cc96140d0f36
mihomihouk <mihomihouk@gmail.com>
2022-04-08 11:42:04 +0100

    Sidebar routing

--- src/components/organisms/Sidebar.jsx
+++ src/components/organisms/Sidebar.jsx
@@ -1,32 +1,81 @@
 import React from 'react'
-
 import { useRecoilState } from "recoil"
+import { useRouter } from 'next/router'
 
 //hooks
 import { navbarState } from "../../hooks/NavbarState"
 
 //styles
-import {  Drawer, List, ListItem, ListItemIcon, ListItemText, Toolbar } from '@mui/material'
-import Dashboard from '@mui/icons-material/Dashboard'
+import {  Drawer, List, ListItem, ListItemIcon, ListItemText, Toolbar, MenuItem } from '@mui/material'
+import HomeIcon from '@mui/icons-material/Home'
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth'
+import ForumIcon from '@mui/icons-material/Forum'
+import TipsAndUpdatesIcon from '@mui/icons-material/TipsAndUpdates'
+
+//components
+import Link from '../../Link'
+
+
 
 const Sidebar = () => {
   const [ open, setOpen ] = useRecoilState(navbarState)
+  const router = useRouter()
+
+  const activeRoute = (routeName, currentRoute) => {
+    return routeName === currentRoute? true : false;
+  }
+
+  const routes = [
+    { 
+      id: 1,
+      title:"Home", 
+      path: "/dashboard",
+      icon:<HomeIcon/> 
+    },
+    {
+      id:2,
+      title:"Calendar",
+      path: "/calendar", 
+      icon:<CalendarMonthIcon/>
+    },
+    {
+      id:3, 
+      title:"Chat", 
+      path: "/chat", 
+      icon:<ForumIcon/> 
+    },
+    {
+      id:4,
+      title:"Discussion", 
+      path: "/discussion", 
+      icon:<TipsAndUpdatesIcon/>
+    }
+  ]
 
   const handleDrawerToggle = () => {
     setOpen(!open)
   }
 
+
+
   const drawer = (
     <>
       <Toolbar/>
       <List>
-        {['Home', 'Calendar', 'Chat', 'Discussion'].map(text => (
-          <ListItem button key={text}>
-            <ListItemIcon>
-              <Dashboard/>
-            </ListItemIcon>
-            <ListItemText primary={text} />
-          </ListItem>
+        {routes.map(item => (
+          <Link 
+            href={item.path}
+            key={item.id} 
+          >
+              <MenuItem selected={activeRoute(item.path, router.pathname)}>
+                <ListItem button>
+                    <ListItemIcon>
+                      {item.icon}
+                    </ListItemIcon>
+                    <ListItemText primary={item.title}/>
+                </ListItem>
+              </MenuItem>
+          </Link>
         ))}
       </List>
     </>
