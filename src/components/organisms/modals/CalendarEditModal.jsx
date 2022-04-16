50dc600bb60eba3e10fa0cca06af9847ecbb40e8
mihomihouk <mihomihouk@gmail.com>
2022-04-16 14:25:48 +0100

    Added update function to calendar

--- src/components/organisms/modals/CalendarEditModal.jsx
+++ src/components/organisms/modals/CalendarEditModal.jsx
@@ -1,20 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 //firebase 
 import { db } from "../../../firebase/config"
-import { doc, deleteDoc } from "firebase/firestore"
+import { doc, deleteDoc, updateDoc } from "firebase/firestore"
 
 //styles
-import { Grid, Typography, Box, Stack, Divider } from "@mui/material"
-import Modal from '@mui/material/Modal'
+import { Grid, Box, Divider, Modal, List, Stack, TextField, Typography } from "@mui/material"
 import NotesIcon from '@mui/icons-material/Notes'
-import LocalOfferIcon from '@mui/icons-material/LocalOffer';
-
+import LocalOfferIcon from '@mui/icons-material/LocalOffer'
 
 //components
+import Title from "../../atoms/inputs/Title"
 import EditButton from "../../atoms/buttons/EditButton"
 import DeleteButton from "../../atoms/buttons/DeleteButton"
 import CloseButton from "../../atoms/buttons/CloseButton"
+import AddButton from '../../atoms/buttons/AddButton'
+import TagSelector from '../../modules/TagSelector'
+import EventTime from '../../modules/EventTime'
 
 const labelsClasses = [
   "indigo",
@@ -28,6 +30,13 @@ const labelsClasses = [
 function CalendarEditModal(props) {
 
   const { open, handleClose, eventToEdit } = props
+
+  const [isEditing, setIsEditing] = useState(false)
+  const [title, setTitle] = useState("")
+  const [startDate, setStartDate] = useState("")
+  const [endDate, setEndDate] = useState("")
+  const [detail, setDetail] = useState("")
+  const [label, setLabel] = useState()
 
   const handleDelete = () => {
     if (
@@ -43,12 +52,43 @@ function CalendarEditModal(props) {
     }
   }
 
+  const handleCloseEdit = () => {
+    setIsEditing(false)
+    setTitle("")
+    setStartDate("")
+    setEndDate("")
+    setDetail("")
+    setLabel("")
+  }
+
+  const handleUpdate = async() => {
+
+    const docRef = doc(db, "events", eventToEdit.id)
+
+    await updateDoc(docRef, {
+      title, 
+      description:detail,
+      start:startDate,
+      end:endDate,
+      label
+    })
+
+    setIsEditing(false)
+    setTitle("")
+    setStartDate("")
+    setEndDate("")
+    setDetail("")
+    setLabel("")
+    handleClose()
+  }
+
   return (
     <>
       <Modal
         open={open}
         onClose={handleClose}
       >
+        {!isEditing ? (
         <Stack spacing={1} sx={{
           position: "absolute",
           top:"50%", left:"50%",
@@ -58,10 +98,10 @@ function CalendarEditModal(props) {
           bgcolor: "background.paper",
           border: "2px solid #000", 
           boxShadow: 24, 
-          p:"3%",
+          p:"3%"
           }}>
           <Box sx={{display:"flex",justifyContent:"flex-end"}}>
-            <EditButton/>
+            <EditButton onClick={() => setIsEditing(true)}/>
             <DeleteButton onClick={handleDelete}/>
             <CloseButton onClick={handleClose}/>
           </Box>
@@ -112,6 +152,76 @@ function CalendarEditModal(props) {
               </Grid>        
             </Grid>
         </Stack>
+        ) : (
+          <Stack spacing={1} sx={{
+            position: "absolute",
+            top:"50%", left:"50%",
+            transform:"translate(-50%,-50%)",
+            width:"40%",
+            height:"70%",
+            bgcolor: "background.paper",
+            border: "2px solid #000", 
+            boxShadow: 24, 
+            p:"3%",
+            }}>
+            <Box sx={{display:"flex",justifyContent:"flex-end"}}>
+              <CloseButton onClick={handleCloseEdit}/>
+            </Box>
+            <Box >
+              <Title title={title} onChange={(e) => setTitle(e.target.value)}/>
+            </Box>
+            <Box fullWidth sx={{display: "flex",alignItems: "center"}}>
+              <Grid container sx={{alignItems: "center"}}>
+                <Grid item xs={2} >
+                  <Typography>From</Typography>
+                </Grid>
+                <Grid item sx={{width:"100%"}} xs={10} >
+                  <EventTime onChange={(newValue)=> setStartDate(newValue)} date={startDate}/>
+                </Grid>
+              </Grid>
+            </Box>
+            <Box fullWidth sx={{display: "flex",alignItems: "center"}}>
+              <Grid container sx={{alignItems: "center"}}>
+                <Grid item xs={2} >
+                  <Typography>To</Typography>
+                </Grid>
+                <Grid item sx={{width:"100%"}} xs={10} >
+                  <EventTime onChange={(newValue)=> setEndDate(newValue)} date={endDate}/>
+                </Grid>
+              </Grid>
+            </Box>
+            <Box sx={{ display: "flex"}} >
+              <Grid container sx={{alignItems: "center"}}>
+                <Grid item xs={1} >
+                  <NotesIcon/>
+                </Grid>
+                <Grid item xs={11} >
+                  <TextField 
+                    label="Description"
+                    fullWidth
+                    value={detail} 
+                    onChange={(e) => setDetail(e.target.value)}
+                  />
+                </Grid>
+              </Grid>
+            </Box>
+            <Box sx={{ display: "flex", alignItems: "center"}} >
+              <Box>
+                <LocalOfferIcon />
+              </Box>
+              <Box >
+                <List sx={{display:"flex"}}>
+                {labelsClasses.map((lblClass, i) => (
+                  <TagSelector lblClass={lblClass} key={i} label={label} onClick={() => setLabel(lblClass)}/>
+                ))}
+                </List>
+              </Box>        
+            </Box>
+            <Box>
+              <AddButton onClick={handleUpdate}/>
+            </Box>
+          </Stack>
+        )}
       </Modal>
     </>
   )
