23b86c4c99b38793f62756441e4ff79cb0fdf404
mihomihouk <mihomihouk@gmail.com>
2022-06-07 14:49:37 +0100

    fixed ts error in useForm

--- src/hooks/useForm.ts
+++ src/hooks/useForm.ts
@@ -4,6 +4,10 @@ type InputData = {
   email?: string;
   password?: string;
   displayName?: string;
+  category?: string;
+  title?: string;
+  detail?: string;
+  newComment?: string;
 };
 const useForm = (initialState = {}) => {
   const [formData, setFormData] = useState<InputData>(initialState);
